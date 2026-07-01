1a351e21e634b929015b61855279d69374d90e69
alwayswin-1 <always.win_1@outlook.com>
2026-07-01 09:14:39 -0700

    fix upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/download/:slug',
+        destination: '/api/download/:slug',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
